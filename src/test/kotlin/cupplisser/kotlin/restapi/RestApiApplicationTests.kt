270df37dad19d6807a9833bf51a2b714990559ac
Muhamad Anjar <arvanria@gmail.com>
2021-03-05 10:15:01 +0700

    initial commit

---
+++ src/test/kotlin/cupplisser/kotlin/restapi/RestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package cupplisser.kotlin.restapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
